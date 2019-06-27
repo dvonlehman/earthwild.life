7aaf76546d8c6dbd9fd88f0b1e63b9d407bcefb8
David Von Lehman <david.vonlehman@gmail.com>
2019-06-26 21:30:49 -0700

    Getting map to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { makeStyles } from "@material-ui/styles";
 import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <LoadScript
-        id="scriptLoader"
-        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY as string}
-      >
+const useStyles = makeStyles({
+  main: {
+    display: "flex"
+  }
+});
+
+const App: React.FC = props => {
+  const classes = useStyles();
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY as string
+  });
+
+  const renderMap = () => {
+    // wrapping to a function is useful in case you want to access `window.google`
+    // to eg. setup options or create latLng object, it won't be available otherwise
+    // feel free to render directly if you don't need that
+    return (
+      <div className={classes.main}>
         <GoogleMap
-          zoom={7}
-          center={{
-            lat: -3.745,
-            lng: -38.523
+          mapContainerStyle={{
+            height: "100vh",
+            width: "100vw"
           }}
+          zoom={5}
+          center={{ lat: 24.886, lng: -70.268 }}
         />
-      </LoadScript>
-    </div>
-  );
+      </div>
+    );
+  };
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>;
+  }
+
+  return isLoaded ? renderMap() : <span>Loading...</span>;
 };
 
 export default App;
