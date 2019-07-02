1cb288738b58321abb88f8ddbd3d5c63ed8152b0
David Von Lehman <david.vonlehman@gmail.com>
2019-07-01 21:07:46 -0700

    Rebuilding all geojson files

---
+++ data/species-spatial.js
@@ -0,0 +1,37 @@
+const LineByLineReader = require("line-by-line");
+const fs = require("fs-extra");
+
+const speciesId = process.argv[2];
+
+const lr = new LineByLineReader(__dirname + "/MAMMALS.geojson");
+
+lr.on("error", function(err) {
+  console.error(err);
+  process.exit(1);
+});
+
+const features = [];
+
+lr.on("line", function(line) {
+  // 'line' contains the current line without the trailing newline character.
+  if (line.includes(`"id_no": ${speciesId}`)) {
+    console.log(`Found line for species_id ${speciesId}`);
+    if (line.endsWith(",")) line = line.slice(0, -1);
+
+    const feature = JSON.parse(line);
+    features.push(feature);
+  }
+});
+
+lr.on("end", function() {
+  fs.writeJsonSync(
+    __dirname + `/${speciesId}.geojson`,
+    {
+      type: "FeatureCollection",
+      features
+    },
+    { spaces: 2 }
+  );
+
+  process.exit();
+});
